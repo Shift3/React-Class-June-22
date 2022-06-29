5dd350859c5c5b8ed377d4351de815b34decee10
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-06-29 19:18:13 -0400

    feat(ea-challenge): Implement get movie detail by id

--- challenges/day1/utils.js
+++ challenges/day1/utils.js
@@ -5,7 +5,15 @@ const getMoviesByName = async(name='Batman') => {
   const response = await fetch(`${API_URL}/?apiKey=${API_KEY}&s=${name}`);
   const data = await response.json();
 
-  console.log('movies:', data);
+  console.log('10 first results of movies:', data);
 }
 
 getMoviesByName('Better call');
+
+const getMovieDetailsById = async (movieId='tt3032476') => {
+  const response = await fetch(`${API_URL}/?apiKey=${API_KEY}&i=${movieId}`);
+  const data = await response.json();
+  console.log('Movie:', data);
+}
+
+getMovieDetailsById();
