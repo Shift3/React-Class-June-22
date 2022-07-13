c6124b80aed7fda082949c56930cd0e8cc2c96c1
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-07-13 18:50:49 -0400

    Provide a solution for take home challenge day 2.

--- challenges/day-1/src/MovieCard.js
+++ challenges/day-1/src/MovieCard.js
@@ -1,22 +1,31 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import "./movieCard.css";
+import {getMoviesByName} from './utils';
 import MovieDetails from "./MovieDetails";
 
-const MovieCard = ({movieId, title, type, posterUrl}) => {
+const MovieCard = () => {
   const [showDetails, setShowDetails] = useState(false);
-  
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    getMoviesByName('Batman').then(response => {
+      setMovie(response.Search[0]);
+    })
+  }, []);
 
   return (
+    !movie ? 'Loading the first movie' : (
     <>
     <div className="container" style={{cursor: "pointer"}} onClick={() => setShowDetails(true)}>
-      <img src={posterUrl} alt={title}/>
-      <p>{title}</p>
-      <span>{type}</span>
+      <img src={movie.Poster} alt={movie.Title}/>
+      <p>{movie.Title}</p>
+      <span>{movie.Type}</span>
     </div>
 
-    {showDetails ? (<MovieDetails movieId={movieId} />): null}
+    {showDetails ? (<MovieDetails movieId={movie.imdbID} />): null}
 
     </>
+    )
   );
 }
 
