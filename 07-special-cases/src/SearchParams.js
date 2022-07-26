0b252cf14164770e7bdbf5fab52056a4213d2c4a
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-07-25 20:46:50 -0400

    Implement the theme context

--- 07-special-cases/src/SearchParams.js
+++ 07-special-cases/src/SearchParams.js
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import Results from "./Results";
 import useBreedList from "./useBreedList";
+import ThemeContext from "./ThemeContext";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -10,6 +11,7 @@ const SearchParams = () => {
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
   const [breeds] = useBreedList(animal);
+  const [theme, setTheme] = useContext(ThemeContext);
 
   useEffect(() => {
     requestPets();
@@ -82,7 +84,17 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-        <button>Submit</button>
+
+        <label htmlFor="theme">
+          Theme:
+          <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+            <option value="peru">Peru</option>
+            <option value="yellow">Yellow</option>
+            <option value="mediumorchid">Medium Orchid</option>
+            <option value="blue">Blue</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
       <br />
 
